b90f4dabf7014c8891278ec70f5e6991ea8deb2e
DRedDevil04 <devamydesai@gmail.com>
2025-04-18 10:33:43 +0530

    add integer decode

--- app/main.go
+++ app/main.go
@@ -34,6 +34,24 @@ func decodeBencode(bencodedString string) (interface{}, error) {
 		}
 
 		return bencodedString[firstColonIndex+1 : firstColonIndex+1+length], nil
+	} else if unicode.IsLetter(rune(bencodedString[0])) {
+		if bencodedString[0] == 'i' {
+			// Handle integer
+			var end int
+			for i := 1; i < len(bencodedString); i++ {
+				if bencodedString[i] == 'e' {
+					end = i
+					break
+				}
+			}
+			intValue, err := strconv.Atoi(bencodedString[1:end])
+			if err != nil {
+				return "", err
+			}
+			return intValue, nil
+		} else {
+			return "", fmt.Errorf("Invalid bencoded string")
+		}
 	} else {
 		return "", fmt.Errorf("Only strings are supported at the moment")
 	}
